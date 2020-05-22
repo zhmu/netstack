fa3cf91e653e3576dec04cde82ec376267d1b100
Rink Springer <rink@rink.nu>
2020-05-22 16:13:43 +0200

    Add iterator-streaming and u32 support to net_order

--- src/netorder.h
+++ src/netorder.h
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <utility>
 
 namespace netstack {
 namespace net_order {
@@ -14,6 +15,36 @@ template<typename Iterator> uint16_t Consume_u16(Iterator& iterator)
 	const auto lo = static_cast<uint16_t>(Consume_u8(iterator));
 	return (hi << 8) | lo;
 }
+
+template<typename Iterator> uint32_t Consume_u32(Iterator& iterator)
+{
+	const auto hi = static_cast<uint32_t>(Consume_u16(iterator));
+	const auto lo = static_cast<uint32_t>(Consume_u16(iterator));
+	return (hi << 16) | lo;
+}
+
+template<typename Iterator>
+struct Consumer
+{
+	Consumer(Iterator&& it) : it(std::move(it)) { }
+
+	Consumer& operator>>(uint8_t& v) {
+		v = Consume_u8(it);
+		return *this;
+	}
+
+	Consumer& operator>>(uint16_t& v) {
+		v = Consume_u16(it);
+		return *this;
+	}
+
+	Consumer& operator>>(uint32_t& v) {
+		v = Consume_u32(it);
+		return *this;
+	}
+
+	Iterator it;
+};
 
 }
 }
--- tests/test_netorder.cpp
+++ tests/test_netorder.cpp
@@ -28,5 +28,24 @@ TEST(NetworkByte, Read_u16)
 	EXPECT_EQ(0xffff, net_order::Consume_u16(it));
 	EXPECT_EQ(data.end(), it);
 }
+
+TEST(NetworkByte, Read_u32)
+{
+	constexpr std::array data{ 0_b, 1_b, 2_b, 3_b, 0x55_b, 0xaa_b, 0xff_b, 0x00_b };
+	auto it = data.begin();
+	EXPECT_EQ(0x00010203, net_order::Consume_u32(it));
+	EXPECT_EQ(0x55aaff00, net_order::Consume_u32(it));
+	EXPECT_EQ(data.end(), it);
+}
+
+TEST(NetworkByte, Consumer)
+{
+	constexpr std::array data{ 0_b, 1_b, 2_b, 3_b, 4_b, 5_b, 6_b };
+	net_order::Consumer consumer(data.begin());
+	{ uint8_t v; consumer >> v; EXPECT_EQ(0x00, v); }
+	{ uint16_t v; consumer >> v; EXPECT_EQ(0x0102, v); }
+	{ uint32_t v; consumer >> v; EXPECT_EQ(0x03040506, v); }
+}
+
 }
 }
