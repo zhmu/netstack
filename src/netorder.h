44434b5b9e873a38cdfc539100cada0f0b11dcfa
Rink Springer <rink@rink.nu>
2020-05-23 15:01:51 +0200

    Add writing bytes in network order

--- src/netorder.h
+++ src/netorder.h
@@ -23,6 +23,23 @@ template<typename Iterator> uint32_t Consume_u32(Iterator& iterator)
 	return (hi << 16) | lo;
 }
 
+template<typename Iterator> void Produce_u8(Iterator& iterator, const uint8_t v)
+{
+	*iterator++ = static_cast<std::byte>(v);
+}
+
+template<typename Iterator> void Produce_u16(Iterator& iterator, const uint16_t v)
+{
+	Produce_u8(iterator, static_cast<uint8_t>(v >> 8));
+	Produce_u8(iterator, static_cast<uint8_t>(v & 0xff));
+}
+
+template<typename Iterator> void Produce_u32(Iterator& iterator, const uint32_t v)
+{
+	Produce_u16(iterator, static_cast<uint16_t>(v >> 16));
+	Produce_u16(iterator, static_cast<uint16_t>(v & 0xffff));
+}
+
 template<typename Iterator>
 struct Consumer
 {
@@ -43,6 +60,33 @@ struct Consumer
 		return *this;
 	}
 
+	Iterator it;
+};
+
+template<typename Iterator>
+struct Producer
+{
+	Producer(Iterator&& it) : it(std::move(it)) { }
+
+	Producer& operator<<(const uint8_t v) {
+		Produce_u8(it, v);
+		bytesProduced += sizeof(uint8_t);
+		return *this;
+	}
+
+	Producer& operator<<(const uint16_t v) {
+		Produce_u16(it, v);
+		bytesProduced += sizeof(uint16_t);
+		return *this;
+	}
+
+	Producer& operator<<(const uint32_t v) {
+		Produce_u32(it, v);
+		bytesProduced += sizeof(uint32_t);
+		return *this;
+	}
+
+	size_t bytesProduced{};
 	Iterator it;
 };
 
--- tests/test_netorder.cpp
+++ tests/test_netorder.cpp
@@ -47,5 +47,48 @@ TEST(NetworkByte, Consumer)
 	{ uint32_t v; consumer >> v; EXPECT_EQ(0x03040506, v); }
 }
 
+TEST(NetworkByte, Write_u8)
+{
+	std::array<std::byte, 2> data{};
+	auto it = data.begin();
+	net_order::Produce_u8(it, 0x12);
+	net_order::Produce_u8(it, 0x34);
+	EXPECT_EQ(data.end(), it);
+	EXPECT_EQ(0x12_b, data[0]);
+	EXPECT_EQ(0x34_b, data[1]);
+}
+
+TEST(NetworkByte, Write_u16)
+{
+	std::array<std::byte, 4> data{};
+	auto it = data.begin();
+	net_order::Produce_u16(it, 0x55aa);
+	net_order::Produce_u16(it, 0xff00);
+	EXPECT_EQ(data.end(), it);
+	EXPECT_TRUE(ranges::equal(std::array{ 0x55_b, 0xaa_b, 0xff_b, 0x00_b }, data));
+}
+
+TEST(NetworkByte, Write_u32)
+{
+	std::array<std::byte, 8> data{};
+	auto it = data.begin();
+	net_order::Produce_u32(it, 0x12345678);
+	net_order::Produce_u32(it, 0xff55aa00);
+	EXPECT_EQ(data.end(), it);
+	EXPECT_TRUE(ranges::equal(std::array{ 0x12_b, 0x34_b, 0x56_b, 0x78_b, 0xff_b, 0x55_b, 0xaa_b, 0x00_b }, data));
+}
+
+TEST(NetworkByte, Producer)
+{
+	std::array<std::byte, 8> data;
+	net_order::Producer producer(data.begin());
+	producer << static_cast<uint8_t>(0x00);
+	producer << static_cast<uint16_t>(0x0102);
+	producer << static_cast<uint32_t>(0x03040506);
+	producer << static_cast<uint8_t>(0x07);
+	EXPECT_EQ(producer.bytesProduced, std::distance(data.begin(), data.end()));
+	EXPECT_TRUE(ranges::equal(std::array{ 0x00_b, 0x01_b, 0x02_b, 0x03_b, 0x04_b, 0x05_b, 0x06_b, 0x07_b }, data));
+}
+
 }
 }
